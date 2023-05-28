6caf54cf47114b376258440417eedaac0663d277
Marcos Barros <81267060+marcosbarross@users.noreply.github.com>
2023-05-28 20:21:37 -0300

    Faz conexão com banco postgres e já é capaz de persistir dados

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,29 +1,8 @@
 package org.example;
-import org.example.models.ProdutoEntity;
 import org.example.util.HibernateUtil;
-import org.hibernate.Session;
-import org.hibernate.Transaction;
 
 public class Main {
     public static void main(String[] args) {
-        ProdutoEntity produtoEntity = new ProdutoEntity();
-        produtoEntity.setName("Nome do Produto");
-        produtoEntity.setPreco(10.99);
-
-        Session session = HibernateUtil.getSessionFactory().openSession();
-        Transaction transaction = null;
-
-        try {
-            transaction = session.beginTransaction();
-            session.persist(produtoEntity);
-            transaction.commit();
-        } catch (Exception ex) {
-            if (transaction != null) {
-                transaction.rollback();
-            }
-            ex.printStackTrace();
-        } finally {
-            session.close();
-        }
+        HibernateUtil.persistirProduto("Produto 2", 58.45);
     }
 }
--- src/main/java/org/example/models/ProdutosEntity.java
+++ src/main/java/org/example/models/ProdutosEntity.java
@@ -1,10 +1,22 @@
 package org.example.models;
 
+import jakarta.persistence.*;
+
 import java.util.Objects;
 
+@Entity
+@Table(name = "produtos", schema = "public", catalog = "AdsEnterpriseDB")
+@SequenceGenerator(name = "produtosSeq", sequenceName = "produtos_seq", allocationSize = 1)
 public class ProdutosEntity {
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "produtosSeq")
+    @Id
+    @Column(name = "id")
     private int id;
+    @Basic
+    @Column(name = "Nome")
     private String nome;
+    @Basic
+    @Column(name = "Preco")
     private double preco;
 
     public int getId() {
