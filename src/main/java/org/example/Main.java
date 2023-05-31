d758bfbfd14e4131eb9e547d7156e5a48294e1d7
root <rafael.luna.cavalcanti@gmail.com>
2023-05-30 22:17:22 -0300

    Revendo conexao com db

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,7 +5,7 @@ import java.math.BigDecimal;
 
 public class Main {
     public static void main(String[] args) {
-        HibernateUtil.persistirProduto("Produto 2", 58.45, 3);
+        HibernateUtil.persistirProduto("Produto 8", 58.45, 3);
 
     }
 }
