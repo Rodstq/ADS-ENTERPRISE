1284de346239b4b1674706bb402daf13f4ebb075
root <rafael.luna.cavalcanti@gmail.com>
2023-06-08 22:29:14 -0300

    adicionado a tabela produtos e a relação com cliente e modificado o tipo de lista para objetos

--- bin/Mysql/init.sql
+++ bin/Mysql/init.sql
@@ -1,6 +1,6 @@
 Create table clientes(
+cpf varchar(15) primary  key,
 nome varchar(50),
-cpf varchar(15) primary  key not null,
 dataNascimento varchar(10),
 telefone varchar(15)
 );
@@ -15,4 +15,22 @@ CREATE TABLE clientesEndereco (
   rua varchar(25),
   cpf varchar(15),
   FOREIGN KEY (cpf) REFERENCES clientes(cpf)
+);
+
+
+CREATE TABLE produtos (
+  idProduto VARCHAR(50) PRIMARY KEY NOT NULL,
+  nomeProduto VARCHAR(50),
+  quantidadeProduto VARCHAR(50),
+  valorProduto VARCHAR(50),
+  dataValidade VARCHAR(50),
+  dataEntrada VARCHAR(50),
+  dataSaida VARCHAR(50)
+);
+
+create table  cliente_produto(
+cpf varchar (15),
+idProduto varchar(50),
+foreign key(cpf) references clientes(cpf),
+foreign key(idProduto) references produtos(idProduto)
 );
---
+++ src/controladores/consultaProdutos.java
@@ -0,0 +1,5 @@
+package controladores;
+
+public class consultaProdutos {
+
+}
